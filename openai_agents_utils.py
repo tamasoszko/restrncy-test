9a9c254dd791b9fcd658b4defdcd7096d8a546ac
Tamas Oszko <taoszko@cisco.com>
2025-10-15 20:57:40 +0200

    update openai chat workflow

---
+++ openai_agents_utils.py
@@ -0,0 +1,60 @@
+
+from typing import Any
+from agents import Agent, AgentHooks, ModelResponse, RunContextWrapper, TResponseInputItem
+
+
+def history_item_to_string(item: str | TResponseInputItem) -> str:
+
+    def _content_item_to_string(content: any) -> str:
+        if isinstance(content, str):
+            return content
+        if isinstance(content, dict) and "text" in content:
+            return content['text']
+        return str(content)
+
+    if isinstance(item, str):
+        return item
+    if "role" not in item or "content" not in item:
+        return str(item)
+    content = item["content"]
+    if isinstance(content, str):
+        return f"{item['role']}: {content}"
+    if isinstance(content, list):
+        content_str = "\n".join(_content_item_to_string(item) for item in content)
+        return f"{item['role']}: {content_str}"
+    return str(content)
+
+
+class LoggerHooks(AgentHooks):
+
+    def __init__(self, log_on_handoff: bool = True, 
+                        log_on_llm_start: bool = False, 
+                        log_on_llm_end: bool = False, 
+                        log_on_agent_start: bool = False, 
+                        log_on_agent_end: bool = False):
+        super().__init__()
+        self.log_on_handoff = log_on_handoff
+        self.log_on_llm_start = log_on_llm_start
+        self.log_on_llm_end = log_on_llm_end
+        self.log_on_agent_start = log_on_agent_start
+        self.log_on_agent_end = log_on_agent_end
+
+    async def on_handoff(self, context: RunContextWrapper[None], agent: Agent[None], source: Agent[None]):
+        if self.log_on_handoff:
+            print(f"[Event] on_handoff: '{source.name}' -> '{agent.name}'")
+
+    async def on_llm_start(self, context: RunContextWrapper[None], agent: Agent[None], system_prompt: str, input_items: list[TResponseInputItem]):
+        if self.log_on_llm_start:
+            print(f"[Event] on_llm_start: '{agent.name}', input_items: '{input_items}'")
+
+    async def on_llm_end(self, context: RunContextWrapper[None], agent: Agent[None], response: ModelResponse):
+        if self.log_on_llm_end:
+            print(f"[Event] on_llm_end: '{agent.name}', output: '{response.final_output}'")
+
+    async def on_agent_start(self, context: RunContextWrapper[None], agent: Agent[None]):
+        if self.log_on_agent_start:
+            print(f"[Event] on_agent_start: '{agent.name}'")
+
+    async def on_agent_end(self, context: RunContextWrapper[None], agent: Agent[None], output: Any):
+        if self.log_on_agent_end:
+            print(f"[Event] on_agent_end: '{agent.name}'")
--- openai_agents_workflow.py
+++ openai_agents_workflow.py
@@ -4,22 +4,28 @@ Main Python file - executable and debuggable
 """
 
 import asyncio
-from typing import Any
 import dotenv
 import os
-from agents import Agent, ModelSettings, RunConfig, Runner, TResponseInputItem, Trace, function_tool, set_trace_processors, set_tracing_disabled, trace, run_demo_loop
+from agents import Agent, HandoffInputData, ModelResponse, ModelSettings, RunConfig, RunContextWrapper, RunItem, Runner, TResponseInputItem, function_tool, handoff, set_tracing_disabled
 from agents.extensions.models.litellm_model import LitellmModel
 from agents.extensions.visualization import draw_graph
+from litellm import dataclass
 import mlflow
+from pydantic import BaseModel
+from agents.extensions.handoff_prompt import RECOMMENDED_PROMPT_PREFIX
+from agents.extensions import handoff_filters
 
+from openai_agents_utils import LoggerHooks, history_item_to_string
 
 dotenv.load_dotenv()
+
 
 
 OPENAI_API_ENDPOINT = os.getenv("OPENAI_API_ENDPOINT")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_MODEL_NAME = os.getenv("OPENAI_MODEL_NAME")
 MLFLOW_TRACING_URL = os.getenv("MLFLOW_TRACING_URL")
+
 
 if MLFLOW_TRACING_URL is not None:
     mlflow.openai.autolog()
@@ -34,6 +40,16 @@ model = LitellmModel( model=OPENAI_MODEL_NAME, api_key=OPENAI_API_KEY, base_url=
 
 set_tracing_disabled(True)
 
+class ResultData(BaseModel):
+    best_joke: str
+    decision_reason: str
+    all_jokes: list[str]
+
+@dataclass
+class WorflowContext:  
+    name: str
+    chat_history: list[TResponseInputItem]
+    last_result: ResultData | None
 
 # Tools
 
@@ -55,13 +71,13 @@ async def joke_generator_tool(topic: str, num_jokes: int = 3) -> str:
         runs.append(Runner.run(agent, input=topic))
     joke_results = await asyncio.gather(*runs)
     jokes = "\n----------\n".join([joke.final_output for joke in joke_results])
-    print("All jokes: \n\n")
+    print("[Trace] All jokes: \n\n")
     for i, joke in enumerate(joke_results):
-        print(f"{i+1}: {joke.final_output}\n")
-    print("\n\n")
-
+        print(f"[Trace] {i+1}: {joke.final_output}\n")
+    print("[Trace] \n\n")
 
     return jokes
+
 
 # Agents
 
@@ -83,6 +99,7 @@ worker_agent = Agent(
 orchestrator_agent = Agent(
     name="orchestrator",
     instructions=(
+        f"{RECOMMENDED_PROMPT_PREFIX}\n"
         "You are a orchestrator agent. Your overall goal is to generate the best joke about the topic."
         "Begin EVERY message with '[Orchestrator]: ' "
         "You are responsible for coordinating the workers, decision maker and presenter."
@@ -92,6 +109,7 @@ orchestrator_agent = Agent(
         "User may want to change something or want to generate new jokes. Perform the same process again." 
         ),
     model=model,
+    hooks=LoggerHooks(),
     tools=[joke_generator_tool, 
         decision_maker_agent.as_tool(
             tool_name="decision_maker",
@@ -100,25 +118,103 @@ orchestrator_agent = Agent(
     ],
 )
 
+chat_summary_agent = Agent(
+    name="chat_summary",
+    instructions=(
+        "You are a chat summarizer agent."
+        "Begin EVERY message with '[Chat Summarizer]: ' "
+        "You will be provided with the chat history and you will need to summarize it."
+        "Generate the summary in the following format:\n"
+        " - topic: <topic> the user requested, should be always present in history, use the last one"
+        " - number of jokes: <number of jokes> the user requested, should be always present in history, use the last one"
+        " - best joke: <best joke> optional, if it is present in the chat history"
+        " - user feedback: <user feedback about the best joke if it was presented> optional, if it is present in the chat history, use the last one"
+    ),
+    model=model,
+)
+
+# Handoff handlers: User Chat -> Orchestrator
+# remove chat history from and add summary as history
+# - call chat_summary_agent to summarize the chat history
+# - save chat history to context
+# - return only the summary as history
+
+async def on_handoff_user_chat_to_orchestrator(ctx: RunContextWrapper[WorflowContext]):
+    print(f"[Trace] ctx={ctx}")
+
+async def handoff_filter_user_chat_to_orchestrator(input: HandoffInputData) -> HandoffInputData:
+    input = handoff_filters.remove_all_tools(input)    
+
+    history = "\n".join(history_item_to_string(item) for item in input.input_history)
+
+    summary = await Runner.run(chat_summary_agent, history)
+    print(f"[Trace] Summary:\n{summary.final_output}")
+
+    run_context = input.run_context
+    run_context.context.chat_history = input.input_history
+
+    input_history = summary.to_input_list()[-1:]
+    return HandoffInputData(
+        input_history=tuple(input_history),
+        pre_handoff_items=(),
+        new_items=(),
+        run_context=run_context,
+    )
+
+async def on_handoff_orchestrator_to_user_chat(ctx: RunContextWrapper[WorflowContext], result: ResultData):
+    print(f"[Trace] ctx={ctx}")
+    ctx.context.last_result = result
+    print(f"[Trace] result={result}")
+
+
+# Handoff handlers: Orchestrator -> User Chat
+# rebuild the chat history and add the last result as a developer message:
+# - get the last result from context and format it as a developer message
+# - get the chat history from context and add the developer message to it
+# - return the rebuild chat history as history
+
+async def handoff_filter_orchestrator_to_user_chat(input: HandoffInputData) -> HandoffInputData:
+    input = handoff_filters.remove_all_tools(input)    
+    
+    run_context = input.run_context
+    last_message = {"role": "developer", "content": f"{{'best_joke': '{run_context.context.last_result.best_joke}', 'decision_reason': '{run_context.context.last_result.decision_reason}', 'all_jokes': '{run_context.context.last_result.all_jokes}'}}" }
+    chat_history = [*input.run_context.context.chat_history, last_message]
+
+    return HandoffInputData(
+        input_history=tuple(chat_history),
+        pre_handoff_items=(),
+        new_items=(),
+        run_context=run_context,
+    )
+
 user_chat_agent = Agent(
     name="user_chat",
     instructions=(
+        f"{RECOMMENDED_PROMPT_PREFIX}\n"
         "You are a user chat agent helping user to generate the best joke about the topic."
         "Begin EVERY message with '[Assistant]: ' "
         "You have two responsibilities:"
-        "1. You need to get information about jokes to be generated. You will neer to get the topic and number of jokes to be generated. "
-        "2. You present the final result -the best joke and the reasoning- to the user and ask if they are satisfied with the result or wants some changes."
+        "1. You need to get information about jokes to be generated. You will need to get the topic and number of jokes to be generated. "
+        "2. You format and present the final result - { 'best_joke': <best_joke>, 'decision_reason': <decision_reason> }- to the user and ask if they are satisfied with the result or wants some changes."
         "If the use is satisfied, say goodbye and tell them to type 'exit' or 'bye' to end the conversation."
-        "You never generate jokes yourself, you will only talk to the user understand what they and if there is enough information hand over to the orchestrator agent." 
+        "You never generate jokes yourself, you will only talk to the user understand what they " 
         "At first welcome the user and then ask for the topic and number of jokes to be generated." 
     ),
     model=model,
-    handoffs=[orchestrator_agent]
+    hooks=LoggerHooks(),
+    tools=[],
+    handoffs=[handoff(
+        agent=orchestrator_agent,
+        input_filter=handoff_filter_user_chat_to_orchestrator,
+        on_handoff=on_handoff_user_chat_to_orchestrator,
+    )]
 )
     
 
+
 async def start_chat_loop() -> str:
     input_items: list[TResponseInputItem] = []
+    ctx = WorflowContext(name="user_chat", chat_history=[], last_result=None)
     while True:
         user_input = input("[User]: ")
         if user_input.strip().lower() in {"exit", "quit", "bye", "bb", "q"}:
@@ -126,8 +222,14 @@ async def start_chat_loop() -> str:
         if not user_input:
             continue
         input_items.append({"role": "user", "content": user_input})
-        orchestrator_agent.handoffs.append(user_chat_agent)
-        result = await Runner.run(user_chat_agent, input_items, run_config=RunConfig(tracing_disabled=False))
+        # add handoff to user chat agent here to avoid circular dependency
+        orchestrator_agent.handoffs.append(handoff(
+            agent=user_chat_agent,
+            input_type=ResultData,
+            input_filter=handoff_filter_orchestrator_to_user_chat,
+            on_handoff=on_handoff_orchestrator_to_user_chat,
+        ))
+        result = await Runner.run(user_chat_agent, input_items, run_config=RunConfig(tracing_disabled=False), context=ctx)
         if result.final_output is not None:
             input_items = result.to_input_list()
             print(result.final_output)
@@ -137,14 +239,14 @@ async def main():
     """
     Main function - entry point of the program
     """
-    print("Starting main program...")  
+    print("[Trace] Starting main program...")  
     draw_graph(user_chat_agent, "openai_agents_workflow")
 
     with mlflow.start_run(run_name="joke_generator"):
         result = await start_chat_loop()
         print(result)
 
-    print("Main program completed successfully!")
+    print("[Trace] Main program completed successfully!")
 
 
 if __name__ == "__main__":
